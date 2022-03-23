ffa6ff3c603432e46e2c494e4e88adc0be95e974
AntoineDefourneau <71023508+AntoineDefourneau@users.noreply.github.com>
2022-03-23 16:56:52 -0400

    Add files via upload

--- generate.php
+++ generate.php
@@ -60,59 +60,6 @@ function generateSentence()
 
 }
 
-function generateSentence_old()
-{
-
-    $singular_nouns = json_decode(file_get_contents("elements/nouns/singular_nouns.json"));
-    $plural_nouns = json_decode(file_get_contents("elements/nouns/plural_nouns.json"));
-    $nouns = array_merge($singular_nouns, $plural_nouns);
-    $sentences = json_decode(file_get_contents("elements/sentences_old.json"));
-
-    $sentence = $sentences[random_int(0, Count($sentences) - 1) ];
-
-    while (strpos($sentence, "%noun%") !== false || strpos($sentence, "%singular_noun%") !== false || strpos($sentence, "%plural_noun%") !== false || strpos($sentence, "%noun_be%") !== false)
-    {
-
-        $sentence = str_replace_first("%singular_noun%", $singular_nouns[random_int(0, Count($singular_nouns) - 1) ], $sentence);
-        $sentence = str_replace_first("%plural_noun%", $plural_nouns[random_int(0, Count($plural_nouns) - 1) ], $sentence);
-        $sentence = str_replace_first("%noun%", $nouns[random_int(0, Count($nouns) - 1) ], $sentence);
-
-        if (strpos($sentence, "%noun_be%") !== false)
-        {
-
-            $rng = random_int(0, (Count($singular_nouns) + Count($plural_nouns)) - 1);
-
-            if ($rng < Count($singular_nouns))
-            {
-
-                $noun_be = $singular_nouns[random_int(0, Count($singular_nouns) - 1) ] . ' is';
-
-            }
-            else
-            {
-
-                $noun_be = $plural_nouns[random_int(0, Count($plural_nouns) - 1) ] . ' are';
-
-            }
-
-            $sentence = str_replace_first("%noun_be%", $noun_be, $sentence);
-        }
-
-    }
-
-    if (strpos($sentence, "%") === false) // Il n'y a plus de parties à compléter
-    {
-        return ucfirst($sentence);
-
-    }
-    else
-    {
-
-        return generateSentence();
-
-    }
-
-}
 
 function str_replace_first($search, $replace, $subject)
 {
--- test.php
+++ test.php
@@ -1,12 +1,7 @@
 <?php
 
     include 'generate.php';
-
-    echo "<b>Old generation example :</b>";
-    echo "<br />";
-    echo generateSentence_old();
-    echo "<br />";
-    echo "<b>New generation example :</b>";
+    echo "<b>Generation example :</b>";
     echo "<br />";
     echo generateSentence();
 
