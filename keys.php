e0956e5c275bb3208d419e8587afe3dd38368883
AntoineDefourneau <71023508+AntoineDefourneau@users.noreply.github.com>
2022-03-23 16:55:54 -0400

    Add files via upload

--- keys.php
+++ keys.php
@@ -1,8 +1,8 @@
 <?php
 
-    define("CONSUMER_KEY", 'TO DEFINE');
-    define("CONSUMER_SECRET", 'TO DEFINE');
-    $access_token = 'TO DEFINE';
-    $access_token_secret = 'TO DEFINE';
+    define("CONSUMER_KEY", '3rBmzJxhLtRaJkmk9sg8g99u4');
+    define("CONSUMER_SECRET", 'r3WuZ0NUmeJOeFRWVo15F5HQRDpa3W5u44YdSLtkNvAb24ueHv');
+    $access_token = '1394990532674396162-N5q2nPCBZyD9aklmglvg4YTVvVcJnI';
+    $access_token_secret = 'UHy4WUKVZMPbefDI5fbznhgAIK9ES1ynFYAmBa6MTE3Cs';
 
 ?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+    include 'generate.php';
+
+    echo "<b>Old generation example :</b>";
+    echo "<br />";
+    echo generateSentence_old();
+    echo "<br />";
+    echo "<b>New generation example :</b>";
+    echo "<br />";
+    echo generateSentence();
+
+?>
